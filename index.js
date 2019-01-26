5bb4a5630e04c7d66b8de3f1d114ca6ccd374df2
Gabe <jflores353@gmail.com>
2019-01-26 16:29:00 -0600

    added index file

---
+++ index.js
@@ -0,0 +1,43 @@
+var http = require('http');
+let users = require("./state").users;
+let server = http.createServer(messageReceived);
+server.listen(8080, console.log(" i am working "));
+
+
+function messageReceived(req, res) {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+
+//Code for users
+if(req.method === "GET" && req.url === "/users"){
+let usersJSON = JSON.stringify(users);
+res.write(usersJSON);
+console.log(`test`);
+}
+
+else if(req.method === "GET" && req.url.indexOf("/users") > -1){
+let id = req.url.split("/");
+let user = users.find(p=>p["id"] == id[2]);
+let usersJSON = JSON.stringify(user);
+res.write(usersJSON);
+}
+
+else if(req.method === "GET" && req.url === "/products"){
+    let productsJSON = JSON.stringify(products);
+    res.write(productsJSON);
+    console.log(`test`);
+    }
+    
+else if(req.method === "GET" && req.url.indexOf("/products") > -1){
+let id = req.url.split("/");
+let product = products.find(p=>p["id"] == id[2]);
+let productsJSON = JSON.stringify(product);
+res.write(productsJSON);
+}
+
+
+else{
+    res.write("Not Found");
+}
+
+res.end();
+}
